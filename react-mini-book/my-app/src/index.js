4d376ea34efd354e1241a4e7911893ab1f7ed9ed
gridGitlab <1548433407@qq.com>
2020-06-30 18:21:26 +0800

    add my-app

---
+++ react-mini-book/my-app/src/index.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+// import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+
+class Title extends Component {
+    render() {
+        return (
+            <div>
+                <h1>mini book</h1>
+            </div>
+        )
+    }
+}
+
+class Header extends Component {
+    render() {
+        return (
+            <div>
+                <Title />
+                <h2>this is a Header</h2>
+            </div>
+        )
+    }
+}
+
+class Main extends Component {
+    render() {
+        return (
+            <div>
+                <h2>this is main content</h2>
+            </div>
+        )
+    }
+}
+
+class Footer extends Component {
+    render() {
+        return (
+            <div>
+                <h2>this is Footer</h2>
+            </div>
+        )
+    }
+}
+
+class Index extends Component {
+    render() {
+        return (
+            <div>
+                <Header></Header>
+                <Main></Main>
+                <Footer></Footer>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    {/* <App /> */}
+    <Index />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
